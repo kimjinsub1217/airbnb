8ef24df989c2abf24555d10625966dc10104ad48
kimjinsub <hikm9985@naver.com>
2022-02-14 16:10:26 +0900

    지도에 마커찍기

--- app/src/main/java/com/example/toyproject014_airbnb/MainActivity.kt
+++ app/src/main/java/com/example/toyproject014_airbnb/MainActivity.kt
@@ -1,21 +1,72 @@
 package com.example.toyproject014_airbnb
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.graphics.ColorUtils
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
 import com.naver.maps.map.BuildConfig
-import com.naver.maps.map.MapView
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var naverMap: NaverMap
+    private lateinit var locationSource:FusedLocationSource
+
     private val mapView: MapView by lazy {
         findViewById<MapView>(R.id.mapView)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView.onCreate(savedInstanceState)
 
+        mapView.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+        naverMap.maxZoom = 18.0
+        naverMap.minZoom = 10.0
+
+        val cameraUpdata = CameraUpdate.scrollTo(LatLng(36.791462128437544, 126.99877379823592))
+        naverMap.moveCamera(cameraUpdata)
+
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+
+        locationSource = FusedLocationSource(this@MainActivity,LOCATION_PERMISSION_REQUEST_CODE )
+        naverMap.locationSource =locationSource
+
+        val marker = Marker()
+        marker.position = LatLng(36.793597908715086, 127.00211281622816)
+        marker.map = naverMap
+        marker.icon =MarkerIcons.BLACK
+        marker.iconTintColor = Color.RED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode != LOCATION_PERMISSION_REQUEST_CODE){
+            return
+        }
+
+        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
+            if(!locationSource.isActivated){
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
     }
 
     override fun onStart() {
@@ -54,6 +105,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-
-
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
 }
